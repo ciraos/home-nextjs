a8aec70b9c9d22ec333ef929bba988a217beac87
ciraos <frank.ka@foxmail.com>
2025-05-26 17:15:14 +0800

    feat: 添加MIT LICENSE

--- src/components/thirdparty/umami.tsx
+++ src/components/thirdparty/umami.tsx
@@ -6,7 +6,7 @@ export default function Umami() {
     useEffect(() => {
         const script = document.createElement("script");
         script.defer = true;
-        script.src = "hhttps://umami.ciraos.top/script.js";
+        script.src = "https://umami.ciraos.top/script.js";
         script.setAttribute("data-website-id", "88508faa-6283-4ced-9e83-cb8b72be27e8");
         document.body.appendChild(script);
         return () => { document.body.removeChild(script); }
